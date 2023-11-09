e453d77e5c1090428428de2656c911554c0828f8
Panos-Stergioulas-Mpolis <panosstergioulasmpolis@gmail.com>
2023-11-09 21:35:04 +0200

    DFS without search vis done

--- pvis/src/App.js
+++ pvis/src/App.js
@@ -1,10 +1,9 @@
 import React from "react";
 import Grid from "./Grid/Grid";
-import "./index.css" 
+import "./index.css";
 import { useState } from "react";
 
 function App() {
-
   const [Sx, setX] = useState(0);
   const [Sy, setY] = useState(0);
 
@@ -15,7 +14,7 @@ function App() {
 
   const [clear, setCLear] = useState(false);
 
-  const [alg, setAlg] = useState("A*")
+  const [alg, setAlg] = useState("A*");
 
   const handleVisualizeClick = () => {
     setShouldVis(true);
@@ -23,116 +22,125 @@ function App() {
 
   const handleClearClick = () => {
     setCLear(true);
-  }
+  };
 
   return (
     <div className="app">
-      <div className='screen'>
-        <div className='Sx input'>
-            <label>Choose start(x): </label>
-            <input
-                type='number'
-                min={0}
-                max={49}
-                defaultValue={Sx}
-                onChange={(e) =>{
-                    setX(e.target.value);
-                    if(e.target.value > 49){
-                        setX(49);
-                    }
-                    if(e.target.value < 0){
-                        setX(0);
-                    }
-                }}
-            />
-        </div>
-        
-        <div className='Sy input'>
-            <label>Choose start(y): </label>
-            <input 
-                type='number'
-                min={0}
-                max={14}
-                defaultValue={Sy}
-                onChange={(e) =>{
-                    setY(e.target.value);
-                    if(e.target.value > 14){
-                        setY(14);
-                    }
-                    if(e.target.value < 0){
-                        setY(0);
-                    }
-                }}
-            />
+      <div className="screen">
+        <div className="Sx input">
+          <label>Choose start(x): </label>
+          <input
+            type="number"
+            min={0}
+            max={49}
+            defaultValue={Sx}
+            onChange={(e) => {
+              setX(e.target.value);
+              if (e.target.value > 49) {
+                setX(49);
+              }
+              if (e.target.value < 0) {
+                setX(0);
+              }
+            }}
+          />
         </div>
 
-        <div className='Tx input'>
-            <label>Choose target(x): </label>
-            <input
-                type='number'
-                min={0}
-                max={49}
-                defaultValue={Tx}
-                onChange={(e) =>{
-                    setTX(e.target.value);
-                    if(e.target.value > 49){
-                        setTX(49);
-                    }
-                    if(e.target.value < 0){
-                        setTX(0);
-                    }
-                }}
-            />
+        <div className="Sy input">
+          <label>Choose start(y): </label>
+          <input
+            type="number"
+            min={0}
+            max={14}
+            defaultValue={Sy}
+            onChange={(e) => {
+              setY(e.target.value);
+              if (e.target.value > 14) {
+                setY(14);
+              }
+              if (e.target.value < 0) {
+                setY(0);
+              }
+            }}
+          />
         </div>
-        
-        <div className='Ty input'>
-            <label>Choose target(y): </label>
-            <input
-                type='number'
-                min={0}
-                max={14}
-                defaultValue={Ty}
-                onChange={(e) =>{
-                    setTY(e.target.value);
-                    if(e.target.value > 14){
-                        setTY(14);
-                    }
-                    if(e.target.value < 0){
-                        setTY(0);
-                    }
-                    
-                }}
-            />
+
+        <div className="Tx input">
+          <label>Choose target(x): </label>
+          <input
+            type="number"
+            min={0}
+            max={49}
+            defaultValue={Tx}
+            onChange={(e) => {
+              setTX(e.target.value);
+              if (e.target.value > 49) {
+                setTX(49);
+              }
+              if (e.target.value < 0) {
+                setTX(0);
+              }
+            }}
+          />
+        </div>
+
+        <div className="Ty input">
+          <label>Choose target(y): </label>
+          <input
+            type="number"
+            min={0}
+            max={14}
+            defaultValue={Ty}
+            onChange={(e) => {
+              setTY(e.target.value);
+              if (e.target.value > 14) {
+                setTY(14);
+              }
+              if (e.target.value < 0) {
+                setTY(0);
+              }
+            }}
+          />
         </div>
 
         <div className="algorithm">
           <label>Algorithm: </label>
-          <select id="algorithm" onChange={(e)=>setAlg(e.target.value)}>
+          <select
+            id="algorithm"
+            onChange={(e) => {
+              setAlg(e.target.value);
+              setShouldVis(false);
+            }}
+          >
             <option>A*</option>
             <option>Dijkstra</option>
             <option>BFS</option>
             <option>DFS</option>
           </select>
         </div>
-        <div>
-           Press D to start and stop drawing
-        </div>
-        <div>
-           Press E to start and stop erasing
-        </div>
+        <div>Press D to start and stop drawing</div>
+        <div>Press E to start and stop erasing</div>
         <button className="btn" onClick={handleVisualizeClick}>
           Visualize
         </button>
 
-        <button className="btn" onClick={()=>window.location.reload(false)}>
+        <button className="btn" onClick={() => window.location.reload(false)}>
           Reset
         </button>
 
         <button className="btn" onClick={handleClearClick}>
           Clear Board
         </button>
-    </div>
-      <Grid sX = {Sx} sY = {Sy} tX = {Tx} tY = {Ty} StastVis={shouldVis} clearBoard={clear} alg = {alg}/>
+      </div>
+      <Grid
+        sX={Sx}
+        sY={Sy}
+        tX={Tx}
+        tY={Ty}
+        StastVis={shouldVis}
+        clearBoard={clear}
+        alg={alg}
+      />
     </div>
   );
 }
--- pvis/src/Grid/Grid.js
+++ pvis/src/Grid/Grid.js
@@ -1,10 +1,10 @@
-import React from 'react'
-import { useState } from 'react';
-import "./grid.css"
-import { useEffect } from 'react';
+import React from "react";
+import { useState } from "react";
+import "./grid.css";
+import { useEffect } from "react";
 
-class Node{
-  constructor(x,y,prev=null,){
+class Node {
+  constructor(x, y, prev = null) {
     this.x = x;
     this.y = y;
     this.prev = prev;
@@ -12,306 +12,349 @@ class Node{
 }
 
 class Algos {
-  BFS(sx,sy,tx,ty,visitedNodes){
+  BFS(sx, sy, tx, ty, visitedNodes) {
     let endNode = null;
-    let startNode = new Node(Number(sx),Number(sy));
+    let startNode = new Node(Number(sx), Number(sy));
     let queue = [];
     queue.push(startNode);
     let node = null;
 
-    while(queue.length > 0){
-
+    while (queue.length > 0) {
       node = queue.shift();
-     
-      if(Number(node.x) === Number(tx) && Number(node.y) === Number(ty)){
+
+      if (Number(node.x) === Number(tx) && Number(node.y) === Number(ty)) {
         endNode = node;
         break;
       }
-      visitedNodes[node.y][node.x] = true;
-      function tryToPushNode(dx,dy){
-        if(
+      visitedNodes[node.x][node.y] = true;
+      function tryToPushNode(dx, dy) {
+        if (
           Number(node.x + dx) <= 49 &&
           Number(node.x + dx) >= 0 &&
           Number(node.y + dy) <= 14 &&
           Number(node.y + dy) >= 0
-          ){
-            if(document.getElementById(`${node.x + dx},${node.y + dy}`).style.backgroundColor !== "black"){
-              if(visitedNodes[node.y + dy][node.x + dx] === false){
-                let nodeChild = new Node(node.x + dx, node.y + dy, node);
-                queue.push(nodeChild);
-                visitedNodes[node.y + dy][node.x + dx] = true;
-              }
+        ) {
+          if (
+            document.getElementById(`${node.x + dx},${node.y + dy}`).style
+              .backgroundColor !== "black"
+          ) {
+            if (visitedNodes[node.x + dx][node.y + dy] === false) {
+              let nodeChild = new Node(node.x + dx, node.y + dy, node);
+              queue.push(nodeChild);
+              visitedNodes[node.x + dx][node.y + dy] = true;
             }
+          }
         }
       }
-      tryToPushNode(0, -1); 
-      tryToPushNode(1, -1); 
-      tryToPushNode(1, 0); 
-      tryToPushNode(1, 1); 
-      tryToPushNode(0, 1); 
-      tryToPushNode(-1, 1); 
-      tryToPushNode(-1, 0); 
+      tryToPushNode(0, -1);
+      tryToPushNode(1, -1);
+      tryToPushNode(1, 0);
+      tryToPushNode(1, 1);
+      tryToPushNode(0, 1);
+      tryToPushNode(-1, 1);
+      tryToPushNode(-1, 0);
       tryToPushNode(-1, -1);
     }
     return endNode;
   }
 
-  DFS(sx,sy,tx,ty,visitedNodes){
+  DFS(sx, sy, tx, ty, visitedNodes) {
     let endNode = null;
-    let node = new Node(sx,sy,null);
-    
-    search(node);
-    function search(node){
-      
-      const list = [];
-      if(Number(node.x) === Number(tx) && Number(node.y) === Number(ty)){
-        console.log(endNode + " a")
-        endNode = node;
-      }
+    let node = new Node(sx, sy, null);
 
-      if(visitedNodes[node.y][node.x]){
+    function search(node) {
+      // Check if endNode is already found
+      if (endNode !== null) {
         return;
       }
-      visitedNodes[node.y][node.x] = true;
-      function tryToPushNode(dx,dy){
-        if(
+
+      const list = [];
+      console.log(node.x + " ok " + node.y);
+      if (node.x == Number(tx) && node.y == Number(ty)) {
+        endNode = node;
+        return; // Stop the search when the endNode is found
+      }
+
+      if (visitedNodes[node.x][node.y]) {
+        console.log(node.x + " aha " + node.y);
+        return;
+      }
+      visitedNodes[node.x][node.y] = true;
+      console.log(node.x + " ihi " + node.y);
+      function tryToPushNode(dx, dy) {
+        if (
           Number(node.x + dx) <= 49 &&
           Number(node.x + dx) >= 0 &&
           Number(node.y + dy) <= 14 &&
           Number(node.y + dy) >= 0
-          ){
-            if(document.getElementById(`${node.x + dx},${node.y + dy}`).style.backgroundColor !== "black"){
-              if(visitedNodes[node.y + dy][node.x + dx] === false){
-                visitedNodes[node.y + dy][node.x + dx] = true;
-                let childNode = new Node(node.x + dx, node.y + dy,node)
-                list.push(childNode);
-              }
+        ) {
+          console.log(node.x + " oho " + node.y);
+          if (
+            document.getElementById(`${node.x + dx},${node.y + dy}`).style
+              .backgroundColor !== "black"
+          ) {
+            if (!visitedNodes[node.x + dx][node.y + dy]) {
+              list.push(new Node(node.x + dx, node.y + dy, node));
             }
+          }
         }
       }
-      tryToPushNode(0, -1); 
-      tryToPushNode(1, -1); 
-      tryToPushNode(1, 0); 
-      tryToPushNode(1, 1); 
-      tryToPushNode(0, 1); 
-      tryToPushNode(-1, 1); 
-      tryToPushNode(-1, 0); 
+
+      tryToPushNode(0, -1);
+      tryToPushNode(1, -1);
+      tryToPushNode(1, 0);
+      tryToPushNode(1, 1);
+      tryToPushNode(0, 1);
+      tryToPushNode(-1, 1);
+      tryToPushNode(-1, 0);
       tryToPushNode(-1, -1);
-      
-      while(list.length > 0){
-        search(list.shift())
+      while (list.length > 0) {
+        search(list.shift());
       }
     }
+
+    search(node);
     return endNode;
   }
 }
 
-  
+class AlgoVis {
+  BFS(i, j, tx, ty, callback) {
+    const graphArray = [];
+    graphArray.push([Number(i), Number(j)]);
 
-class AlgoVis{
+    function processNextStep() {
+      if (graphArray.length > 0) {
+        let node = graphArray.shift();
+        let i = node[0];
+        let j = node[1];
+        if (i === Number(tx) && j === Number(ty)) {
+          callback();
+          return;
+        }
 
-
-    BFS(i, j, tx, ty, callback) {
-      const graphArray = []
-      graphArray.push([Number (i),Number (j)]);
-    
-      function processNextStep() {
-        if (graphArray.length > 0) {
-          
-    
-          let node = graphArray.shift();
-          let i = node[0];
-          let j = node[1];
-          if (i === Number(tx) && j === Number(ty)) {
-            callback();
-            return;
-          }
-    
-          if(document.getElementById(`${i},${j}`).style.backgroundColor !== "blue"){
-            document.getElementById(`${i},${j}`).style.backgroundColor = "lightgreen";
-          }
-          function changeColorAndPush(directionI, directionJ) {
-            if (j + directionJ >= 0 && j + directionJ <= 14 && i + directionI >= 0 && i + directionI <= 49) {
-              let element = document.getElementById(`${i + directionI},${j + directionJ}`);
-              if (
-                element &&
-                element.style.backgroundColor !== "blue" &&
-                element.style.backgroundColor !== "lightgreen" &&
-                element.style.backgroundColor !== "rgb(153, 0, 255)" &&
-                element.style.backgroundColor !== "black" &&
-                element.style.backgroundColor !== "yellow"
-              ) {
-                if(element.style.backgroundColor !== "red"){
-                  element.style.backgroundColor = "yellow";
-                }
-                  graphArray.push([i + directionI, j + directionJ]);
+        if (
+          document.getElementById(`${i},${j}`).style.backgroundColor !== "blue"
+        ) {
+          document.getElementById(`${i},${j}`).style.backgroundColor =
+            "lightgreen";
+        }
+        function changeColorAndPush(directionI, directionJ) {
+          if (
+            j + directionJ >= 0 &&
+            j + directionJ <= 14 &&
+            i + directionI >= 0 &&
+            i + directionI <= 49
+          ) {
+            let element = document.getElementById(
+              `${i + directionI},${j + directionJ}`
+            );
+            if (
+              element &&
+              element.style.backgroundColor !== "blue" &&
+              element.style.backgroundColor !== "lightgreen" &&
+              element.style.backgroundColor !== "rgb(153, 0, 255)" &&
+              element.style.backgroundColor !== "black" &&
+              element.style.backgroundColor !== "yellow"
+            ) {
+              if (element.style.backgroundColor !== "red") {
+                element.style.backgroundColor = "yellow";
               }
+              graphArray.push([i + directionI, j + directionJ]);
             }
           }
-    
-          changeColorAndPush(0, -1); 
-          changeColorAndPush(1, -1); 
-          changeColorAndPush(1, 0); 
-          changeColorAndPush(1, 1); 
-          changeColorAndPush(0, 1); 
-          changeColorAndPush(-1, 1); 
-          changeColorAndPush(-1, 0); 
-          changeColorAndPush(-1, -1);
-    
-          setTimeout(processNextStep, 1); // Continue with the next step after a delay
         }
+
+        changeColorAndPush(0, -1);
+        changeColorAndPush(1, -1);
+        changeColorAndPush(1, 0);
+        changeColorAndPush(1, 1);
+        changeColorAndPush(0, 1);
+        changeColorAndPush(-1, 1);
+        changeColorAndPush(-1, 0);
+        changeColorAndPush(-1, -1);
+
+        setTimeout(processNextStep, 1); // Continue with the next step after a delay
       }
-    
-      processNextStep();
     }
+
+    processNextStep();
+  }
 }
 
 const Grid = (props) => {
+  const [width, setWidth] = useState(50);
+  const [height, setHeight] = useState(15);
 
-    const[width, setWidth] = useState(50);
-    const[height, setHeight] = useState(15);
+  const [startX, setStartX] = useState(props.sX);
+  const [startY, setStartY] = useState(props.sY);
+  const [targetX, setTargetX] = useState(props.tX);
+  const [targetY, setTargetY] = useState(props.tY);
 
-    const [startX, setStartX] = useState(props.sX);
-    const [startY, setStartY] = useState(props.sY);
-    const [targetX, setTargetX] = useState(props.tX);
-    const [targetY, setTargetY] = useState(props.tY);
+  const [draw, setDraw] = useState(false);
+  const [erase, setErase] = useState(false);
 
-    const [draw, setDraw] = useState(false);
-    const [erase, setErase] = useState(false);
-
-    function block(i,j){
-      if(draw === true && !((j === props.sY) && (i === props.sX)) && !((j === props.tY) && (i === props.tX))){
-        document.getElementById(`${i},${j}`).style.backgroundColor = "black";
-      }
-      if(erase === true && !((j === props.sY) && (i === props.sX)) && !((j === props.tY) && (i === props.tX))){
-        document.getElementById(`${i},${j}`).style.backgroundColor = "white";
-      }
+  function block(i, j) {
+    if (
+      draw === true &&
+      !(j === props.sY && i === props.sX) &&
+      !(j === props.tY && i === props.tX)
+    ) {
+      document.getElementById(`${i},${j}`).style.backgroundColor = "black";
     }
+    if (
+      erase === true &&
+      !(j === props.sY && i === props.sX) &&
+      !(j === props.tY && i === props.tX)
+    ) {
+      document.getElementById(`${i},${j}`).style.backgroundColor = "white";
+    }
+  }
 
-    
   useEffect(() => {
     const handleKeyPress = (event) => {
-      if (event.key === 'd' || event.key === 'D') {
+      if (event.key === "d" || event.key === "D") {
         setDraw(!draw);
         setErase(false);
-        
       }
-      if (event.key === 'e' || event.key === 'E') {
+      if (event.key === "e" || event.key === "E") {
         setErase(!erase);
         setDraw(false);
-        
       }
-      
     };
 
-    document.addEventListener('keydown', handleKeyPress);
+    document.addEventListener("keydown", handleKeyPress);
 
     return () => {
-      document.removeEventListener('keydown', handleKeyPress); // Clean up the event listener
+      document.removeEventListener("keydown", handleKeyPress); // Clean up the event listener
     };
   }, [draw, erase]);
 
-    const arrayOfNodes = [];
-    const visitedNodes = [];
+  const arrayOfNodes = [];
+  const visitedNodes = [];
 
-function setGrid(){
-     for(let i = 0; i < height; i++){
+  function setGrid() {
+    for (let i = 0; i < height; i++) {
       let tempArr = [];
-      visitedNodes[i] = []; 
-      for(let j = 0; j < width; j++){
-        tempArr.push(<div onMouseMove={()=>block(j,i)} className={"node"} id={`${j},${i}`}></div>);
-        visitedNodes[i][j] = false; 
+      visitedNodes[i] = [];
+      for (let j = 0; j < width; j++) {
+        tempArr.push(
+          <div
+            onMouseMove={() => block(j, i)}
+            className={"node"}
+            id={`${j},${i}`}
+          ></div>
+        );
+        visitedNodes[i][j] = false;
       }
-      arrayOfNodes.push(<div className='line'>{tempArr}</div>);
+      arrayOfNodes.push(<div className="line">{tempArr}</div>);
     }
-}
-
-
-    setGrid();
-
-    useEffect(()=>{
-     
-    },[props.clearBoard])
-    
-
-    useEffect(() =>{  
-      async function change(){
-        document.getElementById(`${startX},${startY}`).style.background = "white";
-        document.getElementById(`${targetX},${targetY}`).style.background = "white";
+    for (let i = 0; i < width; i++) {
+      visitedNodes[i] = [];
+      for (let j = 0; j < height; j++) {
+        visitedNodes[i][j] = false;
       }
+    }
+  }
 
-      change()
-      setStartX(props.sX);
-      setStartY(props.sY);
-      setTargetX(props.tX);
-      setTargetY(props.tY);
-    },[props.sX, props.sY, props.tX, props.tY])
+  setGrid();
 
-    useEffect(() =>{
-      async function change(){
-        document.getElementById(`${startX},${startY}`).style.background = "linear-gradient(240deg, red, purple, green, yellow, blue, black, white, orange)"
-        document.getElementById(`${targetX},${targetY}`).style.background = "linear-gradient(240deg,  red, purple, green, yellow, blue, black, white, magenta)"
+  useEffect(() => {}, [props.clearBoard]);
+
+  useEffect(() => {
+    async function change() {
+      document.getElementById(`${startX},${startY}`).style.background = "white";
+      document.getElementById(`${targetX},${targetY}`).style.background =
+        "white";
+    }
+
+    change();
+    setStartX(props.sX);
+    setStartY(props.sY);
+    setTargetX(props.tX);
+    setTargetY(props.tY);
+  }, [props.sX, props.sY, props.tX, props.tY]);
+
+  useEffect(() => {
+    async function change() {
+      document.getElementById(`${startX},${startY}`).style.background =
+        "linear-gradient(240deg, red, purple, green, yellow, blue, black, white, orange)";
+      document.getElementById(`${targetX},${targetY}`).style.background =
+        "linear-gradient(240deg,  red, purple, green, yellow, blue, black, white, magenta)";
+    }
+
+    change();
+  }, [startX, startY, targetX, targetY]);
+
+  useEffect(() => {
+    let node = null;
+
+    async function Visualize() {
+      let g = new AlgoVis();
+      let alg = new Algos();
+      if (props.alg === "BFS") {
+        g.BFS(startX, startY, targetX, targetY, () => {
+          // This callback is called when BFS traversal is complete
+          node = alg.BFS(startX, startY, targetX, targetY, visitedNodes);
+          let pathArr = [];
+          pathArr.push(node);
+          while (node.prev !== null) {
+            pathArr.push(node.prev);
+            node = node.prev;
+          }
+          visualizePath(pathArr);
+        });
+      } else if (props.alg === "DFS") {
+        node = alg.DFS(
+          Number(startX),
+          Number(startY),
+          Number(targetX),
+          Number(targetY),
+          visitedNodes
+        );
+        console.log(node);
+        if (node) {
+          let pathArr = [];
+          pathArr.push(node);
+          while (node.prev !== null) {
+            pathArr.push(node.prev);
+            node = node.prev;
+          }
+          visualizePath(pathArr);
+        } else {
+          console.log("No path found.");
+        }
       }
+    }
 
-      change()
-    },[startX, startY, targetX, targetY])
-
-    useEffect(() => {
-      let node = null;
-    
-      async function Visualize() {
-        let g = new AlgoVis();
-        let alg = new Algos();
-        if (props.alg === "BFS") {
-          g.BFS(startX, startY, targetX, targetY, () => {
-            // This callback is called when BFS traversal is complete
-            node = alg.BFS(startX, startY, targetX, targetY, visitedNodes);
-            let pathArr = [];
-            pathArr.push(node);
-            while (node.prev !== null) {
-              pathArr.push(node.prev);
-              node = node.prev;
+    async function visualizePath(arr) {
+      while (arr.length > 0) {
+        let node = arr.pop();
+        let i = Number(node.x);
+        let j = Number(node.y);
+        await new Promise((resolve) => {
+          setTimeout(() => {
+            if (
+              document.getElementById(`${i},${j}`).style.backgroundColor !==
+                "red" &&
+              document.getElementById(`${i},${j}`).style.backgroundColor !==
+                "blue"
+            ) {
+              document.getElementById(`${i},${j}`).style.backgroundColor =
+                "rgb(153, 0, 255)";
             }
-            visualizePath(pathArr);
-          });
-        } else if (props.alg === "DFS") {
-          node = alg.DFS(startX, startY, targetX, targetY, visitedNodes);
-          console.log(node + "aa");
-          visualizePath(node);
-        }
+            resolve();
+          }, 10);
+        });
       }
-    
-      async function visualizePath(arr) {
-      
-        while (arr.length > 0) {
-          let node = arr.pop();
-          let i = Number(node.x);
-          let j = Number(node.y);
-          await new Promise((resolve) => {
-            setTimeout(() => {
-              if (
-                document.getElementById(`${i},${j}`).style.backgroundColor !== "red" &&
-                document.getElementById(`${i},${j}`).style.backgroundColor !== "blue"
-              ) {
-                document.getElementById(`${i},${j}`).style.backgroundColor = "rgb(153, 0, 255)";
-              }
-              resolve();
-            }, 10);
-          });
-        }
-      }
-    
-      if (props.StastVis) {
-         Visualize();
-      }
-    }, [props.StastVis]);
-    
-    
-  return (
-    <div className='grid'>
-        {arrayOfNodes}
-    </div>
-  )
-}
+    }
 
-export default Grid
+    if (props.StastVis) {
+      Visualize();
+    }
+  }, [props.StastVis]);
+
+  return <div className="grid">{arrayOfNodes}</div>;
+};
+
+export default Grid;
