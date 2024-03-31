b2316a17831b1b1fac1ecc0fe4eb14717d1a5ae7
Panos-Stergioulas-Mpolis <panosstergioulasmpolis@gmail.com>
2024-03-31 18:13:14 +0300

    Complete BUG-0 logic

--- pvis/src/algorithms/BUG0.js
+++ pvis/src/algorithms/BUG0.js
@@ -24,7 +24,7 @@ export const BUG0 = (
   speed
 ) => {
   let endNode = null;
-  let totalNodesVisited = 1;
+  let totalNodesVisited = 0;
   const startTime = performance.now();
   let curreNode = new Node(Number(sx), Number(sy), null, 0);
   let goal = new Node(Number(tx), Number(ty), null, 0);
@@ -77,7 +77,26 @@ export const BUG0 = (
       }
       totalNodesVisited++;
 
+      let previus = curreNode;
       let d = directionToTarget(curreNode, goal.x, goal.y);
+      let dir = "";
+      if (d.x === curreNode.x && d.y > curreNode.y) {
+        dir = "Down";
+      } else if (d.x === curreNode.x && d.y < curreNode.y) {
+        dir = "Up";
+      } else if (d.x > curreNode.x && d.y === curreNode.y) {
+        dir = "Right";
+      } else if (d.x < curreNode.x && d.y === curreNode.y) {
+        dir = "Left";
+      } else if (d.x < curreNode.x && d.y < curreNode.y) {
+        dir = "Left-Up";
+      } else if (d.x < curreNode.x && d.y > curreNode.y) {
+        dir = "Left-down";
+      } else if (d.x > curreNode.x && d.y < curreNode.y) {
+        dir = "Right-Up";
+      } else if (d.x > curreNode.x && d.y > curreNode.y) {
+        dir = "Right-down";
+      }
 
       if (canMove(d, width, height)) {
         curreNode = d;
@@ -86,17 +105,91 @@ export const BUG0 = (
         if (Currdirection === null) {
           Currdirection = d;
         }
-        while (true) {
-          // console.log(curreNode);
-          // console.log(Currdirection);
-          if (canMove(Currdirection, width, height)) {
-            let prev = curreNode;
-            curreNode = Currdirection;
-            curreNode.prev = prev;
-            Currdirection = rotateRight(prev, Currdirection);
-            break;
-          } else {
-            Currdirection = rotateLeft(curreNode, Currdirection);
+
+        mySecLoop();
+        async function mySecLoop() {
+          await timer(speed);
+          while (true) {
+            // console.log(curreNode);
+            // console.log(Currdirection);
+            if (canMove(Currdirection, width, height)) {
+              let prev = curreNode;
+              curreNode = Currdirection;
+              curreNode.prev = prev;
+              Currdirection = rotateRight(prev, Currdirection);
+              if (
+                (curreNode.x !== sx || curreNode.y !== sy) &&
+                (curreNode.x !== tx || curreNode.y !== ty)
+              ) {
+                let element = document.getElementById(
+                  `${curreNode.x},${curreNode.y}`
+                );
+
+                element.animate(
+                  [
+                    {
+                      transform: "scale(.5)",
+                      background: "rgb(255, 60, 16)",
+                      opacity: ".8",
+                    },
+                    {
+                      transform: "scale(1.5)",
+                      background: "rgb(237, 20, 16)",
+                      opacity: "1.5",
+                    },
+                    {
+                      transform: "scale(1)",
+                      background: "rgb(237, 60, 16)",
+                      opacity: "1",
+                    },
+                  ],
+                  {
+                    duration: 500,
+                    easing: "ease-in-out",
+                    fill: "backwards",
+                  }
+                );
+
+                element.style.backgroundColor = "rgb(237, 60, 16)";
+              }
+              totalNodesVisited++;
+
+              if (dir === "Up") {
+                if (curreNode.y > previus.y) {
+                  break;
+                }
+              } else if (dir === "Down") {
+                if (curreNode.y < previus.y) {
+                  break;
+                }
+              } else if (dir === "Left") {
+                if (curreNode.x < previus.x) {
+                  break;
+                }
+              } else if (dir === "Right") {
+                if (curreNode.x > previus.x) {
+                  break;
+                }
+              } else if (dir === "Left-Up") {
+                if (curreNode.x < previus.x && curreNode.y < previus.y) {
+                  break;
+                }
+              } else if (dir === "Left-down") {
+                if (curreNode.x < previus.x && curreNode.y > previus.y) {
+                  break;
+                }
+              } else if (dir === "Right-Up") {
+                if (curreNode.x > previus.x && curreNode.y < previus.y) {
+                  break;
+                }
+              } else if (dir === "Right-down") {
+                if (curreNode.x > previus.x && curreNode.y > previus.y) {
+                  break;
+                }
+              }
+            } else {
+              Currdirection = rotateLeft(curreNode, Currdirection);
+            }
           }
         }
       }
